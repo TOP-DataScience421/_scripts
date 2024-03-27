abfb743d31e3055e6247f52fdc65ec758fbc10a5
Gennadiy <renardes@gmail.com>
2024-03-27 23:55:12 +0500

    преобразование в тип int

---
+++ base/types1.py
@@ -0,0 +1,17 @@
+print('int(2.02) =', int(2.02))
+print('int(4.98) =', int(4.98))
+
+print('int(False) =', int(False))
+print('int(True) =', int(True))
+
+print('int("135") =', int("135"))
+print('int("-35") =', int("-35"))
+
+print('int("1111", 2) =', int("1111", 2))
+print('int("ff", 16) =', int("ff", 16))
+
+# приведёт к ValueError
+# print('int("9.2") =', int("9.2"))
+# print('int("7 4") =', int("7 4"))
+# print('int("h2") =', int("h2"))
+
