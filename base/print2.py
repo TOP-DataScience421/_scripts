5747487bd9209649751e1518e946f45b926ed583
Gennadiy <renardes@gmail.com>
2024-03-27 21:42:56 +0500

    параметр sep функции print()

---
+++ base/print2.py
@@ -0,0 +1,15 @@
+print('в', 'лесу', 'родилась', 'ёлочка')
+print('в', 'лесу', 'она', 'росла', end='\n\n')
+
+print('1', '2', '3', '4', sep='__', end='\n\n')
+
+print(
+    'student 1', 
+    'student 2', 
+    'student 3', 
+    'student 4', 
+    'student 5', 
+    'student 6', 
+    sep='\n'
+)
+
