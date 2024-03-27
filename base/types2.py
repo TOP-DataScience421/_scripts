638270f8442bdedd3b1be018eda1f64f4e7a810e
Gennadiy <renardes@gmail.com>
2024-03-27 23:55:26 +0500

    преобразование в тип float

---
+++ base/types2.py
@@ -0,0 +1,15 @@
+print('float(2) =', float(2))
+
+print('float(False) =', float(False))
+print('float(True) =', float(True))
+
+print('float(".8") =', float(".8"))
+print('float("9.2") =', float("9.2"))
+print('float("-24.1") =', float("-24.1"))
+print('float("86") =', float("86"))
+
+# приведёт к ValueError
+# print('float("6,3") =', float("6,3"))
+# print('float("7 4") =', float("7 4"))
+# print('float("9a") =', float("9a"))
+
