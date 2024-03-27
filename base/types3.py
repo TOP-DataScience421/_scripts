a0baa1405c5f6097d6bf6fe4c65ad79adc9594c4
Gennadiy <renardes@gmail.com>
2024-03-27 23:56:02 +0500

    явная проверка на истинность

---
+++ base/types3.py
@@ -0,0 +1,12 @@
+print('bool(2) =', bool(2))
+print('bool(-2) =', bool(-2))
+print('bool(0) =', bool(0))
+
+print('bool(0.0000000001) =', bool(0.0000000001))
+print('bool(-12.6) =', bool(-12.6))
+print('bool(0.0) =', bool(0.0))
+
+print('bool("abc") =', bool("abc"))
+print('bool(" ") =', bool(" "))
+print('bool("") =', bool(""))
+
