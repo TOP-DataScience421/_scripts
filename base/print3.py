47170203f0edc9582e4edd1526a5f0eab7371759
Gennadiy <renardes@gmail.com>
2024-03-27 21:43:11 +0500

    типы аргументов функции print()

---
+++ base/print3.py
@@ -0,0 +1,10 @@
+a = 1
+b = 2.03
+c = 'слово'
+d = [1, 2, 'we']
+e = {'key1': 'value1', 'key2': 'value2'}
+
+print(a, b, c, d, e)
+
+# 1 2.03 слово [1, 2, 'we'] {'key1': 'value1', 'key2': 'value2'}
+
