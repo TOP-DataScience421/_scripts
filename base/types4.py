fffb856ca0051d21d76d6f29dca06d103e28ffc8
Gennadiy <renardes@gmail.com>
2024-03-27 23:56:30 +0500

    обращение к человекочитаемому строковому представлению объектов

---
+++ base/types4.py
@@ -0,0 +1,12 @@
+print('str(2) =', str(2))
+print('str(-2) =', str(-2))
+print('str(0) =', str(0))
+
+print('str(0.0000000001) =', str(0.0000000001))
+print('str(-12.6) =', str(-12.6))
+print('str(0.0) =', str(0.0))
+
+print('str("abc") =', str("abc"))
+print('str(" ") =', str(" "))
+print('str("") =', str(""))
+
