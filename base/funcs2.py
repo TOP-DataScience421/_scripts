482310549a343dac3eee0d6c6a99067030cf3fc4
Gennadiy <renardes@gmail.com>
2024-04-21 10:58:37 +0500

    возвращаемые значения функций

---
+++ base/funcs2.py
@@ -0,0 +1,26 @@
+def adder():
+    print(2 + 2)
+
+
+def divider():
+    return 1 / 7
+
+
+# >>> adder()
+# 4
+# >>>
+# >>> result1 = adder()
+# 4
+# >>>
+# >>> type(result1)
+# <class 'NoneType'>
+
+
+# >>> divider()
+# 0.14285714285714285
+# >>>
+# >>> result2 = divider()
+# >>>
+# >>> type(result2)
+# <class 'float'>
+
